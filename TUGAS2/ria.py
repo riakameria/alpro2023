3a3379454c14e1d2c3229e64e921e31b3bb31701
riakameria <kameria.22315031@umkendari.ac.id>
2023-10-20 10:41:26 +0700

    tugas 2 selesai

---
+++ TUGAS2/ria.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+	# tuple
+	x = (1, "Belimbing", 3.14, "Ceri")
+	# list
+	y = [1, "Belimbing", 3.14, "Ceri"]
+	# dictionary
+	z ={
+		1: "Apel", "b": "Belimbing", "c": "Ceri", "x": "Xemangka",
+	}
+	print(type(x))
+	print(x)
+	print(type(y))
+	print(y)
+	print(type(z))
+	print(z)
