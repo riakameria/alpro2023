f74d529d2f36b07fa27307a2c15902f0c028f275
riakameria <148076135+riakameria@users.noreply.github.com>
2023-11-12 22:04:35 +0700

    tugas 3
    selesai

---
+++ TUGAS3/tugas.3-luas-persegi.py
@@ -0,0 +1,6 @@
+
+
+if __name__ == "__main__":
+	input_str = input("masukkan angka: ")
+	angka = int(input_str)
+	print ("sisa bagi angka {} adalah: {}" .format (angka, angka%2))
---
+++ TUGAS3/tugas.3_ganjil-genap.py
@@ -0,0 +1,6 @@
+
+
+if __name__ == "__main__":
+	input_str = input("masukan angka: ")
+	angka =int(input_str)
+	print ("sisa bagi angka {} adalah: {}" .format(angka, angka%2))
